99bcb50b6b02c875bbed58092781fd1acc32a945
GLOBBIMUS <striker1342566@gmail.com>
2019-03-04 23:50:19 -0600

    Updates

---
+++ precache-manifest.415a78e96cf4cc2d995563d5239a0be8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f74da163ed06f53007fef4bee8b1d6ab",
+    "url": "/static/media/my_background_4.f74da163.png"
+  },
+  {
+    "revision": "1893bf148830c9a5fca8e7c6ea91a984",
+    "url": "/static/media/coop_pic3.1893bf14.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5170f4782afa5cb1d9f",
+    "url": "/static/js/main.d5170f47.chunk.js"
+  },
+  {
+    "revision": "1a821169fe340e37f36a",
+    "url": "/static/js/1.1a821169.chunk.js"
+  },
+  {
+    "revision": "d5170f4782afa5cb1d9f",
+    "url": "/static/css/main.d2510004.chunk.css"
+  },
+  {
+    "revision": "cbaed071f89988e5e1f59105a716dece",
+    "url": "/index.html"
+  }
+];
